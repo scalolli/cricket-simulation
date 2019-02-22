57b4cf1e9cdf53ab0b531bf82636ec4a0b149a02
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2019-02-22 15:58:02 +0000

    Add test to validate that we generate 2's less than 3's

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,4 +5,5 @@ import Control.Monad.Random
 main :: IO ()
 main = hspec $ do
     it "returns numbers based on their probability" $ do
-           (fst $ take 10 (generateRandomScore [(1,0), (2,2), (3,3)] (mkStdGen 1))) `shouldBe` [2,3]
+          values <- return (take 10 $ fst $ (generateRandomScore [(1,0), (2,2), (3,3)] (mkStdGen 1)))
+          (length $ filter (==2) values) `shouldSatisfy` (< (length $ filter (==3) values))
